4bdf5d96e4d9225f874ef394f1c1b891437dbaac
Bernd Heidemann <bernd.heidemann@googlemail.com>
2022-11-01 10:46:58 +0100

    Lösung RandomNumbers und VolumeFilter

--- src/randomNumbers/RandomNumbers.java
+++ src/randomNumbers/RandomNumbers.java
@@ -2,16 +2,43 @@ package randomNumbers;
 
 import java.util.Random;
 
-public class RandomNumbers{
+public class RandomNumbers {
     public int[] getRandomNumbers(int number) {
-        return null;
+        Random generator = new Random();
+        // Array anlegen mit der korrekten Anzahl an Elementen
+        int[] response = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            // eine Zufallszahl zwischen 1 und 10 erzeugen
+            int numberBetweenOneAndTen = generator.nextInt(10);
+            // 0,1,2,3,4,5,6,7,8,9
+            numberBetweenOneAndTen += 1;
+            // 1,2,3,4,5,6,7,8,9,10
+
+            // Zufallszahl in die korrekte stelle im Array speichern
+            response[i] = numberBetweenOneAndTen;
+        }
+        return response;
     }
 
-    public int evaluateArray(int[] randomNumbers, int digit){
-        return 0;
+    public int evaluateArray(int[] randomNumbers, int digit) {
+        int counter = 0;
+        for (int i = 0; i < randomNumbers.length; i++) {
+            if (randomNumbers[i] == digit) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
-    public String getRandomNumbersToString(int[] randomNumbers){
-        return null;
+    public String getRandomNumbersToString(int[] randomNumbers) {
+        String response = "";
+        for (int i = 0; i < randomNumbers.length; i++) {
+            // Die Zahl an der Stelle i wird zu response hinzugefügt
+            response += randomNumbers[i];
+            // Leerzeichen hinzufügen
+            response += " ";
+        }
+        return response;
     }
 }
--- src/volumeFilter/VolumeFilter.java
+++ src/volumeFilter/VolumeFilter.java
@@ -3,6 +3,38 @@ package volumeFilter;
 public class VolumeFilter {
 
     public int[] smoothValues(int[] values) {
-        return null;
+
+        int[] response = new int[values.length];
+
+        // den ersten Wert berechnen
+        // Berechne für das erste Element von
+        // geglaettet den Durchschnitt der ersten
+        // zwei Elemente von signal.
+        int first = values[0];
+        int second = values[1];
+        response[0] = (first + second) / 2;
+
+        // den letzten Wert berechnen
+        int indexOfLastElement = values.length - 1;
+        int last = values[indexOfLastElement];
+        int indexOfSecondLastElement = values.length - 2;
+        int secondLast = values[indexOfSecondLastElement];
+        response[values.length - 1] = (last + secondLast) / 2;
+
+        // alle anderen Werte berechnen
+        // Schleife vom zweiten bis zum vorletzten Index
+        for (int i = 1; i <= values.length - 2; i++) {
+            // Vorgänger
+            int prev = values[i - 1];
+            // den aktuellen
+            int current = values[i];
+            // den Nachfolger
+            int next = values[i + 1];
+
+            int avg = (prev + current + next) / 3;
+            response[i] = avg;
+        }
+
+        return response;
     }
 }
