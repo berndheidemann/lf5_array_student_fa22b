b4bc17b132cf751174ba2dab47e072bfc4de3913
Bernd Heidemann <bernd.heidemann@googlemail.com>
2022-11-15 11:10:11 +0100

    ArraySum2D und GameOfLife

--- src/arraySum2d/ArraySum2d.java
+++ src/arraySum2d/ArraySum2d.java
@@ -2,14 +2,76 @@ package arraySum2d;
 
 public class ArraySum2d {
     public int sumAll(int[][] array) throws IllegalArgumentException {
-        return 0;
+        if (array == null || array.length == 0 || array[0].length == 0) {
+            throw new IllegalArgumentException();
+        }
+        // Die Summe aller Werte in einem 2D-Array zu berechnen
+        int sum = 0;
+        // Schleifenkonstrukt, dass alle Werte im 2D-Array einmal anspricht
+        // Durch alle Reihen gehen
+        for (int row = 0; row < array.length; row++) {
+            // Durch alle Spalten einer Reihe gehen
+            int colCount = array[row].length; // --> Anzahl der Spalten dieser einen Reihe
+            for (int col = 0; col < colCount; col++) {
+                sum += array[row][col];
+            }
+        }
+
+        // Alternative mit for each-Schleife
+//        for(int[] row: array) {    // for(EINZELNES ELEMENT: CONTAINER)
+//            for(int value: row) {
+//                sum+=value;
+//            }
+//        }
+        return sum;
     }
 
+
+    /*
+                                3 | 5 | 1
+                                1 | 2 | 1
+                                4 | 2 | 3
+
+                                array[1][0]
+
+     */
     public int[] rowSums(int[][] array) throws IllegalArgumentException {
-        return null;
+        if (array == null || array.length == 0 || array[0].length == 0) {
+            throw new IllegalArgumentException();
+        }
+        // Ein 1D-Array mit den Summen der einzelnen Reihen
+        int[] sum = new int[array.length];
+
+        // sum ---> 0 | 0 | 0
+        for (int row = 0; row < array.length; row++) {
+            // Durch alle Spalten einer Reihe gehen
+            int colCount = array[row].length; // --> Anzahl der Spalten dieser einen Reihe
+            for (int col = 0; col < colCount; col++) {
+                sum[row] += array[row][col];
+            }
+        }
+        // sum --> 9 | 4 | 9
+        return sum;
     }
+
 
     public int[] colSums(int[][] array) throws IllegalArgumentException {
-        return null;
+        if (array == null || array.length == 0 || array[0].length == 0) {
+            throw new IllegalArgumentException();
+        }
+        // Ein 1D-Array mit den Summen der einzelnen Spalten
+        // wie viele Spalten haben wir eigentlich?
+        int[] sum = new int[array[0].length];
+
+        // sum ---> 0 | 0 | 0
+        for (int row = 0; row < array.length; row++) {
+            // Durch alle Spalten einer Reihe gehen
+            int colCount = array[row].length; // --> Anzahl der Spalten dieser einen Reihe
+            for (int col = 0; col < colCount; col++) {
+                sum[col] += array[row][col];
+            }
+        }
+        // sum --> 9 | 4 | 9
+        return sum;
     }
 }
--- src/gameOfLife/GameOfLife.java
+++ src/gameOfLife/GameOfLife.java
@@ -1,45 +1,150 @@
 package gameOfLife;
 
+import java.util.Random;
+
 public class GameOfLife {
 
-	public void initializeBoard(int number) {
+    private static final char ALIVE = 'O';
+    private static final char DEAD = ' ';
+    private static final int WIDTH = 10;
+    private static final int HEIGHT = 10;
 
-	}
+    char[][] gamefield;
 
-	public char[][] getBoard() {
-		char[][] result =
-				{
-						{'O', 'O', 'O','O','O','O','O','O','O','O'},
-						{'O', 'O', 'O','O','O','O','O','O','O','O'},
-						{'O', 'O', 'O','O','O','O','O','O','O','O'},
-						{'O', 'O', 'O','O','O','O','O','O','O','O'},
-						{'O', 'O', 'O','O','O','O','O','O','O','O'},
-						{'O', 'O', 'O','O','O','O','O','O','O','O'},
-						{'O', 'O', 'O','O','O','O','O','O','O','O'},
-						{'O', 'O', 'O','O','O','O','O','O','O','O'},
-						{'O', 'O', 'O','O','O','O','O','O','O','O'},
-						{'O', 'O', 'O','O','O','O','O','O','O','O'}
-				};
-		return result;
-	}
+    public void initializeBoard(int number) {
+        // Ein Board 12x12 erstellt werden
+        // wobei die Zellen nur in den Indizes von 1 bis 10 existieren
+        char[][] field = new char[HEIGHT][WIDTH];
+        Random rnd = new Random();
+        int spawned = 0;
+        while (spawned < number) {
+            int row = rnd.nextInt(HEIGHT);
+            int col = rnd.nextInt(WIDTH);
+            if (field[row][col] != ALIVE) {
+                field[row][col] = ALIVE;
+                spawned++;
+            }
+        }
+        gamefield = field;
+    }
 
-	public boolean setAliveCell(int row, int col) {
-		return true;
-	}
+    public char[][] getBoard() {
+        return gamefield;
+    }
 
-	public char getCellStatus(int row, int col) {
-		return '-';
-	}
+    public boolean setAliveCell(int row, int col) {
+        row--;
+        col--;
+        if (gamefield[row][col] != ALIVE) {
+            gamefield[row][col] = ALIVE;
+            return true;
+        }
+        return false;
+    }
 
-	public int getAliveCells() {
-		return 0;
-	}
+    public char getCellStatus(int row, int col) {
+        row--;
+        col--;
+        return gamefield[row][col];
+    }
 
-	public void simulate() {
+    public int getAliveCells() {
+        // zähle alle lebenden Zellen
+        int sum = 0;
+        for (int row = 0; row < HEIGHT; row++) {
+            for (int col = 0; col < WIDTH; col++) {
+                if (gamefield[row][col] == ALIVE) {
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
 
-	}
+    public void simulate() {
+        // einen GameOfLife-Step
 
-	public boolean simulationOver() {
-		return false;
-	}
+        /*
+            Variante 1: Die Zelle ist am Leben
+                Regel a: bei mehr als 3 Nachbarn tot
+                Regel b: bei weniger als 2 Nachbar tot
+
+            Variante 2: Die Zelle ist tot
+                Regel: bei exakt 3 Nachbarn --> leben entsteht
+
+            Ich muss für jede Zelle zählen, wieviele Nachbarn sie hat
+         */
+
+        char[][] tempField = new char[HEIGHT][WIDTH];
+
+        for (int row = 0; row < HEIGHT; row++) {
+            for (int col = 0; col < WIDTH; col++) {
+
+                int neighbours = 0;
+
+                // nachbarn berechnen
+
+                // oben
+                if (row > 0 && gamefield[row - 1][col] == ALIVE) {
+                    neighbours++;
+                }
+
+                // oben rechts
+                if (row > 0 && col < WIDTH - 1 && gamefield[row - 1][col + 1] == ALIVE) {
+                    neighbours++;
+                }
+
+                // rechts
+                if (col < WIDTH - 1 && gamefield[row][col + 1] == ALIVE) {
+                    neighbours++;
+                }
+
+                // unten rechts
+                if (row < HEIGHT - 1 && col < WIDTH - 1 && gamefield[row + 1][col + 1] == ALIVE) {
+                    neighbours++;
+                }
+
+                // unten
+                if (row < HEIGHT - 1 && gamefield[row + 1][col] == ALIVE) {
+                    neighbours++;
+                }
+
+                // unten links
+                if (row < HEIGHT - 1 && col > 0 && gamefield[row + 1][col - 1] == ALIVE) {
+                    neighbours++;
+                }
+
+                // links
+                if (col > 0 && gamefield[row][col - 1] == ALIVE) {
+                    neighbours++;
+                }
+
+                //oben links
+                if (row > 0 && col > 0 && gamefield[row - 1][col - 1] == ALIVE) {
+                    neighbours++;
+                }
+
+                // regeln anwenden
+                if (gamefield[row][col] == ALIVE) {
+                    if (neighbours > 3 || neighbours < 2) {
+                        tempField[row][col] = DEAD;
+                    } else {
+                        tempField[row][col] = ALIVE;
+                    }
+                } else {
+                    if (neighbours == 3) {
+                        tempField[row][col] = ALIVE;
+                    } else {
+                        tempField[row][col] = DEAD;
+                    }
+                }
+            }
+        }
+        gamefield = tempField;
+
+    }
+
+    public boolean simulationOver() {
+        return false;
+    }
 }
